b330fbbb7b687346172fce5f031db7555bace50d
unknown <alekscherkasov13@gmail.com>
2022-06-03 12:12:03 +0300

    fill: implement pagination by pages with react-pagination

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,33 +6,34 @@ import PizzasBlock from '../components/pizzasBlock';
 import Skeleton from '../components/sceleton';
 import Sort from '../components/sort';
 import { Context } from '../context';
+import Pagination from '../pagination';
 import { changeCategory, sortCategory } from '../redux/slices/filterSlice';
 
 const URL = 'https://628e644ea339dfef87ad6fce.mockapi.io/pizzas';
 const URL_UKR = 'https://628e644ea339dfef87ad6fce.mockapi.io/pizzas_ukr';
 
 function Home() {
-	//Redux
-	const { isUkraine } = useSelector((state) => state.language);
-  const {categoryId, sortType} = useSelector((state) => state.filter);
-  const {searchValue} = useContext(Context);
-
+  //Redux
+  const { isUkraine } = useSelector((state) => state.language);
+  const { categoryId, sortType } = useSelector((state) => state.filter);
+  const { searchValue } = useContext(Context);
   const dispatch = useDispatch();
-	//State
-	// const [categoriesById, setCategoriesById] = useState(0);
+  //State
+  // const [categoriesById, setCategoriesById] = useState(0);
 
   // const [sortType, setSortType] = useState({ name: 'популярности (A-Z)', sortProperty: 'rating' });
   const [pizzas, setPizzas] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+
   const category = categoryId > 0 ? `category=${categoryId}` : '';
   const sort = sortType['sortProperty'].replace('-', '');
   const order = sortType.sortProperty.startsWith('-') ? 'desc' : 'asc';
   const search = searchValue ? `&search=${searchValue}` : '';
 
-
   useEffect(() => {
     setIsLoading(true);
-    fetch(`${isUkraine ? URL_UKR : URL}?${category}&sortBy=${sort}&order=${order}${search}`)
+    fetch(`${isUkraine ? URL_UKR : URL}?page=${currentPage}&limit=4&${category}&sortBy=${sort}&order=${order}${search}`)
       .then((response) => response.json())
       .then((data) => {
         setTimeout(() => {
@@ -40,16 +41,16 @@ function Home() {
           setIsLoading(false);
         }, 1000);
       });
-  }, [categoryId, sortType,searchValue]);
+  }, [categoryId, sortType, searchValue, currentPage]);
 
-  // const handleChangeCategory = (id) => dispatch(changeCategory(id));
-  // const handleChangeSortType = (obj) => dispatch(sortCategory(obj));
+
+
   return (
     <>
       <div className="container">
         <div className="content__top">
-          <Categories isUkraine={isUkraine}  />
-          <Sort isUkraine={isUkraine}  />
+          <Categories isUkraine={isUkraine} />
+          <Sort isUkraine={isUkraine} />
         </div>
         <h2 className="content__title">{isUkraine ? 'Усі піци ​​💙💛🇺🇦​' : 'Все пиццы ​🌊​🚢​'}</h2>
         <div className="content__items">
@@ -57,6 +58,7 @@ function Home() {
             ? [...new Array(pizzas.length || 6)].map((_, index) => <Skeleton key={index} />)
             : pizzas.map((pizza) => <PizzasBlock key={pizza.id} {...pizza} />)}
         </div>
+        <Pagination onChangePage={(number) => setCurrentPage(number)}/>
       </div>
     </>
   );
